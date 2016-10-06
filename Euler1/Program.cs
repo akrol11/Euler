ad67c15c63cc8694fecf341ea3ad9e3736432358
akrol11 <akrol11@users.noreply.github.com>
2016-10-06 12:19:54 -0500

    test commit

---
+++ Euler1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            string str;
+            str = Console.ReadLine();
+          
+            int param = 0;
+            param = Int32.Parse(str);
+            for (int i = 1; i < param; i++)
+            {
+                if (i%3 == 0 || i%5 == 0)
+                    sum += i;
+            }
+            Console.WriteLine("Result = {0}", sum);
+            Console.ReadLine();
+        }
+    }
+}
